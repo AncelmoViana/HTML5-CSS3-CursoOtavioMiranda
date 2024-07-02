8851ce1a464e853b1bac8a46b531243c5e688ffa
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-07-02 18:18:00 -0300

    update

---
+++ Treinamentos/pag-magnesita/assets/js/script.js
@@ -0,0 +1,28 @@
+
+
+let nomeConta = document.getElementById('nome-conta');
+
+function pt(){
+    let pt=document.getElementById('pt');
+
+    pt.style.background = '#003262';
+    pt.style.color = '#ffffff';
+
+    nomeConta.innerHTML = 'Nome da Conta';
+}
+function en(){
+    let en=document.getElementById('en');
+    
+    
+    en.style.background = '#003262';
+    en.style.color = '#ffffff';
+
+    nomeConta.innerHTML = 'Usuário';
+}
+function es(){
+    let es = document.getElementById('es');
+    
+    es.style.background = '#003262';
+    es.style.color = '#ffffff';
+}
+
