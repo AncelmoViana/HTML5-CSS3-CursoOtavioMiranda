4d0c532f697d9233bd2e174f8cae0cb55e46c039
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-07-03 19:10:39 -0300

    update

--- Treinamentos/pag-magnesita/assets/js/script.js
+++ Treinamentos/pag-magnesita/assets/js/script.js
@@ -1,25 +1,43 @@
+$(document).ready(function(){
+    $('input[class="nome"]').attr('placeholder', );
+    $('input[class="senha"]').attr('placeholder', 'testando senha');
+})
 
 
+let btnEN = document.getElementById('en');
+let btnPT = document.getElementById('pt');
+let btnES = document.getElementById('es');
 
 
 function pt(){
-    let en = document.getElementById('form-en');
-    let pt = document.getElementById('form-pt');
-    en.style.zIndex = '1';
-    pt.style.zIndex = '2';
-    
+    btnEN.style.background = '#ffffff';
+    btnEN.style.color = '#003262';
+    btnPT.style.background = '#003262';
+    btnPT.style.color = '#ffffff';
+    btnES.style.background = '#ffffff';
+    btnES.style.color = '#003262';
+   
+  
     
     
 }
 function en(){
-    let en = document.getElementById('form-en');
-    let pt = document.getElementById('form-pt');
-    en.style.zIndex = '2';
-    pt.style.zIndex = '1';
-    
+
+    btnEN.style.background = '#003262'
+    btnEN.style.color = '#ffffff'
+    btnPT.style.background = '#ffffff'
+    btnPT.style.color = '#003262'
+    btnES.style.background = '#ffffff';
+    btnES.style.color = '#003262';
     
 }
 function es(){
+    btnES.style.background = '#003262';
+    btnES.style.color = '#ffffff';
+    btnEN.style.background = '#ffffff';
+    btnEN.style.color = '#003262'
+    btnPT.style.background = '#ffffff'
+    btnPT.style.color = '#003262'
    
 }
 
