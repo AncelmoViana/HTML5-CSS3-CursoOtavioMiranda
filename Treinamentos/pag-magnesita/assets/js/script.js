49a5fc9d8caf31516d9aeee330af5a99af94f9c1
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-07-03 17:29:12 -0300

    updated

--- Treinamentos/pag-magnesita/assets/js/script.js
+++ Treinamentos/pag-magnesita/assets/js/script.js
@@ -1,87 +1,25 @@
 
 
-let nomeConta = document.getElementById('nome-conta');
-let senha = document.getElementById('senha');
-let esqueciMeuUsuario = document.getElementById('esqueciMeuUsuario');
-let esqueciMinhaSenha = document.getElementById('esqueciMinhaSenha');
-
-
 
 
 function pt(){
-    let pt = document.getElementById('pt');
-    let en=document.getElementById('en'); 
-    let es = document.getElementById('es');
-
-    pt.style.background = '#003262';
-    pt.style.color = '#ffffff';
-
-    en.style.background = '#ffffff';
-    en.style.color = '#003262';
-   
+    let en = document.getElementById('form-en');
+    let pt = document.getElementById('form-pt');
+    en.style.zIndex = '1';
+    pt.style.zIndex = '2';
     
-    es.style.background = '#ffffff';
-    es.style.color = '#003262';
-
-    nomeConta.innerHTML = 'Nome da Conta';
-    nomeConta.style.width = '90px';
-
-    senha.innerHTML = 'Senha';
-    senha.style.width = '48px';
-
-    esqueciMeuUsuario.innerHTML = 'Esqueci meu usuário';
-    esqueciMinhaSenha.innerHTML = 'Esqueci minha senha';
-    $('#nome'). attr ('placeholder', 'testando placeholder novo');
-  
+    
     
 }
 function en(){
-    let pt = document.getElementById('pt');
-    let en=document.getElementById('en'); 
-    let es = document.getElementById('es');
-    
-    pt.style.background = '#ffffff';
-    pt.style.color = '#003262';
+    let en = document.getElementById('form-en');
+    let pt = document.getElementById('form-pt');
+    en.style.zIndex = '2';
+    pt.style.zIndex = '1';
     
     
-    en.style.background = '#003262';
-    en.style.color = '#ffffff';
-
-    es.style.background = '#ffffff';
-    es.style.color = '#003262';
-    
-
-    nomeConta.innerHTML = 'Login';
-    nomeConta.style.width = '40px';
-
-    senha.innerHTML = 'Password';
-    senha.style.width = '70px';
-
-    esqueciMeuUsuario.innerHTML = 'Forgot my username ';
-    esqueciMeuUsuario.style.textAlign = 'right';
-    esqueciMeuUsuario.style.marginRight = '25px';
-    esqueciMeuUsuario.style.color = '#3D5567';
-
-    esqueciMinhaSenha.innerHTML = 'Forgot my password';
-    esqueciMinhaSenha.style.textAlign = 'right';
-    esqueciMinhaSenha.style.marginRight = '25px';
-    esqueciMinhaSenha.style.color = '#3D5567';
-   
 }
 function es(){
-    let pt = document.getElementById('pt');
-    let en=document.getElementById('en'); 
-    let es = document.getElementById('es');
-
-    pt.style.background = '#ffffff';
-    pt.style.color = '#003262';
-    
-    
-    en.style.background = '#ffffff';
-    en.style.color = '#003262';
    
-    
-    es.style.background = '#003262';
-    es.style.color = '#ffffff';
 }
 
