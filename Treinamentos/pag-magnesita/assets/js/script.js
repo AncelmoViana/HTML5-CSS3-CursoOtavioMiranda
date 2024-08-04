d156dc9a14ae3b190e61595e33018b951a3407bd
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-08-04 00:27:22 -0300

    update

--- Treinamentos/pag-magnesita/assets/js/script.js
+++ Treinamentos/pag-magnesita/assets/js/script.js
@@ -6,6 +6,17 @@ let btnEs = document.getElementById('es');
 //variaveis dos nomes dos inputs
 let nomeConta = document.getElementById('nome-conta');
 let senha = document.getElementById('senha');
+//variaveis esqueci meu usuario e esqueci minha senha
+let esqueciUsuario = document.getElementById('esqueciUsuario');
+let esqueciSenha = document.getElementById('esqueciSenha');
+//variaveis botoes de entrar e link final
+let entrar = document.getElementById('entrar');
+let entrarSSO = document.getElementById('entrar-sso');
+let linkFinal = document.getElementById('link-final');
+
+// variavel formulario
+let formulario = document.getElementById('formulario');
+
 
 function pt(){
     //comandos para mudar a cor dos botoes lang ativados
@@ -18,7 +29,26 @@ function pt(){
     btnEs.style.backgroundColor = "#ffffff";
     btnEs.style.color = "#003262";
 
+    //comandos para alterar o idioma dos texos do formulario.
+   nomeConta.innerHTML = 'Nome da Conta';
+   nomeConta.style.width = '90px';
+
+   senha.innerHTML = 'Senha';
+   senha.style.width = '45px';
+
+   esqueciUsuario.innerHTML = 'Esqueci meu usuário';
+   esqueciSenha.innerHTML = 'Esqueci minha  senha';
+
+   entrar.innerHTML = 'ENTRAR';
+   entrarSSO.innerHTML = 'ENTRAR COM SSO';
+
+   linkFinal.style.display = 'block';
+   formulario.style.height = '675px';
+
+
 }
+
+
 
 function en(){
 
@@ -35,8 +65,18 @@ function en(){
    //comandos para alterar o idioma dos texos do formulario.
    nomeConta.innerHTML = 'Login';
    nomeConta.style.width = '40px';
+
    senha.innerHTML = 'password';
    senha.style.width = '65px';
+
+   esqueciUsuario.innerHTML = 'Forgot my username';
+   esqueciSenha.innerHTML = 'Forgot my password';
+
+   entrar.innerHTML = 'ENTER';
+   entrarSSO.innerHTML = 'ENTER WITH SSO';
+
+   linkFinal.style.display = 'none';
+   formulario.style.height = '655px';
 
 
 }
@@ -51,4 +91,22 @@ function es(){
  
     btnEs.style.backgroundColor = "#003262";
     btnEs.style.color = "#ffffff";
+
+    
+    //comandos para alterar o idioma dos texos do formulario.
+   nomeConta.innerHTML = 'Usuario';
+   nomeConta.style.width = '50px';
+
+   senha.innerHTML = 'Contraseña';
+   senha.style.width = '70px';
+
+   esqueciUsuario.innerHTML = 'Olvidé mi usuario';
+   esqueciSenha.innerHTML = 'Olvidé mi contraseña';
+
+   entrar.innerHTML = 'ENTRAR';
+   entrarSSO.innerHTML = 'ENTRAR CON SSO';
+
+   linkFinal.style.display = 'none';
+   formulario.style.height = '655px';
+
 }
