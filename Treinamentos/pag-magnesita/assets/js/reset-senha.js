e432c5e5c0f62e8ad3ab9ee4b389c36ed7c49d06
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-09-05 08:31:10 -0300

    updated

--- Treinamentos/pag-magnesita/assets/js/reset-senha.js
+++ Treinamentos/pag-magnesita/assets/js/reset-senha.js
@@ -14,3 +14,12 @@ function mascaraCPF() {
     }
 }
 
+let iconCorretoPeopleid = document.getElementById('correto-peopleID');
+let iconCorretoCpf = document.getElementById('correto-cpf');    
+let iconCorretoNascimento = document.getElementById('correto-nascimento');
+let iconCorretoAdmissao = document.getElementById('correto-admissao')
+
+let iconErroCpf = document.getElementById('error-cpf');    
+let iconErroNascimento = document.getElementById('error-nascimento');
+let iconErroAdmissao = document.getElementById('error-admissao')
+
