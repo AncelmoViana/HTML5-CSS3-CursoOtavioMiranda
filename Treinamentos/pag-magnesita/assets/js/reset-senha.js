ba9ea92e6e92089654516d034276c24229bf3a66
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-09-05 09:30:38 -0300

    updated

--- Treinamentos/pag-magnesita/assets/js/reset-senha.js
+++ Treinamentos/pag-magnesita/assets/js/reset-senha.js
@@ -3,17 +3,6 @@ let nascimento = document.querySelector('#nascimento');
 let admissao = document.querySelector('#admissao');
 let cpf = document.getElementById("cpf");
 
-function mascaraCPF() {
-    if(cpf.value.length == 3 || cpf.value.length == 7){
-        cpf.value += "."
-
-    }
-    else if(cpf.value.length == 11){
-        cpf.value += "-"
-
-    }
-}
-
 let iconCorretoPeopleid = document.getElementById('correto-peopleID');
 let iconCorretoCpf = document.getElementById('correto-cpf');    
 let iconCorretoNascimento = document.getElementById('correto-nascimento');
@@ -21,5 +10,17 @@ let iconCorretoAdmissao = document.getElementById('correto-admissao')
 
 let iconErroCpf = document.getElementById('error-cpf');    
 let iconErroNascimento = document.getElementById('error-nascimento');
-let iconErroAdmissao = document.getElementById('error-admissao')
+let iconErroAdmissao = document.getElementById('error-admissao');
+
+function mascaraCPF(){
+    if(cpf.value.length == 3 || cpf.value.length == 7){
+        cpf.value += '.';
+    }
+    if(cpf.value.length == 11){
+        cpf.value += '-';
+    }
+
+}
+
+
 
