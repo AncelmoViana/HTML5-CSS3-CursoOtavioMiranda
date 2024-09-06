a25fee7cc7aca0e2d45003f57e593945a0f29d2d
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-09-06 10:13:52 -0300

    updated

--- Treinamentos/pag-magnesita/assets/js/reset-senha.js
+++ Treinamentos/pag-magnesita/assets/js/reset-senha.js
@@ -2,6 +2,7 @@
 let nascimento = document.querySelector('#nascimento');
 let admissao = document.querySelector('#admissao');
 let cpf = document.getElementById("cpf");
+let peopleID = document.getElementById('PeopleID');
 
 let iconCorretoPeopleid = document.getElementById('correto-peopleID');
 let iconCorretoCpf = document.getElementById('correto-cpf');    
@@ -14,16 +15,26 @@ let iconErroAdmissao = document.getElementById('error-admissao');
 
 
 
-
+function validarpeopeID(){
+    if(peopleID.value.trim() !== '' ){
+        iconCorretoPeopleid.style.display = 'flex';
+    }
+    else{
+        iconCorretoPeopleid.style.display = 'none';
+    }
+}
 
 function validarCPF(){
 
-
+  
     if(cpf.value.trim() !== '' && cpf.value.length < 14){
         iconErroCpf.style.display = 'flex';
+        iconCorretoCpf.style.display = 'none';
     }
+
     else{
         iconErroCpf.style.display = 'none';
+        iconCorretoCpf.style.display = 'flex';
     }
 
 }
